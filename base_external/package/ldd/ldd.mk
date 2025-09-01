a13c9e7096c4714b0c9a1ca2c2e6696d7b7d90af
vijaykum <vijaykum@Ubuntu.myguest.virtualbox.org>
2025-09-01 08:24:32 +0530

    Assignment7 part2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 LDD_VERSION = 1.0
-LDD_SITE = /home/vijaykum/Assignment7       
+#LDD_SITE = /home/vijaykum/Assignment7   
+LDD_SITE = $(BR2_EXTERNAL)/package/ldd
 LDD_SITE_METHOD = local
 LDD_LICENSE = GPL-2.0
 LDD_LICENSE_FILES = COPYING
@@ -41,45 +42,6 @@ define LDD_INSTALL_TARGET_CMDS
 		M=$(LDDBUILDDIR)/scull \
 		INSTALL_MOD_PATH=$(TARGET_DIR) modules_install
 endef
-
-
-# We only need to build, no configure
-
-
-#define LDD_BUILD_CMDS
-#	# Make sure headers are prepared (harmless if already done)
-#	$(MAKE) -C $(LINUX_DIR) \
-#		ARCH=$(KERNEL_ARCH) CROSS_COMPILE="$(TARGET_CROSS)" \
-#		modules_prepare
-#
-#	# Build misc-modules
-#	$(MAKE) -C $(LINUX_DIR) \
-#		ARCH=$(KERNEL_ARCH) CROSS_COMPILE="$(TARGET_CROSS)" \
-#		M=$(@D)/misc-modules modules
-#
-#	# Build scull
-#	$(MAKE) -C $(LINUX_DIR) \
-#		ARCH=$(KERNEL_ARCH) CROSS_COMPILE="$(TARGET_CROSS)" \
-#		M=$(@D)/scull modules
-#endef
-
-#define LDD_INSTALL_TARGET_CMDS
-#	# Install misc-modules
-#	$(MAKE) -C $(LINUX_DIR) \
-#		ARCH=$(KERNEL_ARCH) CROSS_COMPILE="$(TARGET_CROSS)" \
-#		M=$(@D)/misc-modules \
-#		DEPMOD=$(HOST_DIR)/sbin/depmod \
-#		INSTALL_MOD_PATH=$(TARGET_DIR) INSTALL_MOD_DIR=extra \
-#		modules_install
-#
-#	# Install scull
-#	$(MAKE) -C $(LINUX_DIR) \
-#		ARCH=$(KERNEL_ARCH) CROSS_COMPILE="$(TARGET_CROSS)" \
-#		M=$(@D)/scull \
-#		DEPMOD=$(HOST_DIR)/sbin/depmod \
-#		INSTALL_MOD_PATH=$(TARGET_DIR) INSTALL_MOD_DIR=extra \
-#		modules_install
-#endef
 
 $(eval $(generic-package))
 
